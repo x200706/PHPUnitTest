2dfb6a23096975f84a57eb173125e909c4566932
Crystal <99391710+x200706@users.noreply.github.com>
2024-07-05 08:34:23 +0000

    try catch測試

--- except.php
+++ except.php
@@ -20,18 +20,63 @@
 // 好啊 456再也不會被執行
 
 // 而且遇到的情境不是其他func catch過了
+// try{
+//   $arr = [];
+//   $arr[] = 123;
+//   $arr[] = 456;
+//   foreach($arr as $item){
+//     if($item == 123){
+//       throwThenCatch();
+//     }
+//     var_dump($item); // 嗯？？456有執行啊 跟你說連123都執行= =
+//     /*
+//     丟丟丟int(123)
+//     int(456)
+//     */
+//   }
 
+// }catch(\Exception $e){
+//   echo $e->getMessage();
+// }
+
+
+// function throwThenCatch(){
+//   try{
+//     throw new InvalidArgumentException('丟丟丟');
+//   }catch(InvalidArgumentException $e){
+//     echo $e->getMessage();
+//   }
+// }
+
+// 但我有個假說 如果catch區塊又有例外呢？寄信套件損壞或rollback引發的例外（誰叫你事務亂寫）
 try{
   $arr = [];
   $arr[] = 123;
   $arr[] = 456;
   foreach($arr as $item){
     if($item == 123){
-      throw new InvalidArgumentException('丟丟丟');
+      throwThenCatch();
     }
-    var_dump($item);
+    var_dump($item); // 嗯？？456有執行啊 跟你說連123都執行= =
+    /*
+    丟丟丟int(123)
+    int(456)
+    */
   }
 
 }catch(\Exception $e){
   echo $e->getMessage();
+  throw new InvalidArgumentException('catch裡面丟丟丟'); // 會變成說沒有人catch欸...寶寶你去哪了🥺
+  // 其實我不確定Laravel command會不會跟API一樣有個最後的catcher（可能要翻有無人時做）
+  // 但如果有 我log也要看到例外才是啊🤔
+  // 無論如何程式直接去世都令人困惑
+}
+
+
+function throwThenCatch(){
+  try{
+    throw new InvalidArgumentException('丟丟丟');
+  }catch(InvalidArgumentException $e){
+    echo $e->getMessage();
+  }
 }
--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 
 $out_side_arr = [];
 
+// private 沒有class包的PHP func不能用欸...好像在說廢話齁...沒物件哪有私有公有的概念...
 function pushElementIntoOutSideArr(&$out_side_arr) { // 指針解
   $out_side_arr[] = 123;
 }
